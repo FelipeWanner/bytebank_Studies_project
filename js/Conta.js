87536f497baa3c1edebba0a3b2a8b0a472e60900
Felipe Rafael Wanner <felipe.w.pakua@gmail.com>
2021-04-14 14:59:43 -0300

    adding abstract method sacar

--- js/Conta.js
+++ js/Conta.js
@@ -1,8 +1,15 @@
+// classe Abstrata (são classes que servem apenas de )
 export class Conta {
     constructor(saldoInicial = 0, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+
+        //esse if verifica em qual classe esse constructor está sendo instanciado, e como não quero que Conta
+        // seja instanciada, se constructor for da Conta, ele lança um erro no console.
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar a classe Conta diretamente, pois essa é uma classe abstrata.");
+        }
     }
 
     depositar(valor) {
@@ -12,9 +19,14 @@ export class Conta {
         this._saldo += valor;
     }
 
+    //sacar é um metodo abstrato.
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+
+        throw new Error("Esse metodo é abstrato e deve ser sobrescrito na classe filha.");
+
+        //Abaixo exemplo de utilização para sobrescrever a classe:
+        // let taxa = 1;
+        // return this._sacar(valor, taxa);
     }
 
     _sacar(valor, taxa){
--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 import{Cliente} from "./Cliente.js";
 import{ContaCorrente} from "./ContaCorrente.js";
 import{ContaPoupanca} from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const conta1 = new ContaCorrente(12, new Cliente("Ricardo",11122233309),1001);
 
 const conta2 = new ContaCorrente(0, new Cliente("Alice",88822233309),1001);
 
-const contaPoupanca = new ContaPoupanca(20, conta1._cliente, conta1._agencia);
+const contaPoupancaRicardo = new ContaPoupanca(20, conta1._cliente, conta1._agencia);
+
+const contaSalarioAlice = new ContaSalario(0, conta2._cliente, conta2._agencia);
 
 conta1.depositar(500);
 conta1.transferir(250, conta2);
@@ -14,6 +17,7 @@ conta2.depositar(350.25);
 
 console.log(conta1);
 console.log(conta2);
-console.log(contaPoupanca);
+console.log(contaPoupancaRicardo);
+console.log(contaSalarioAlice);
 console.log(conta1.sacar(200).toFixed(2));
-console.log(contaPoupanca.sacar(10));
+console.log(contaPoupancaRicardo.sacar(10));
