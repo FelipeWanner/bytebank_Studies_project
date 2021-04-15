2aca9e913d5589590a65bdf0b9af6be882fc02ed
Felipe Rafael Wanner <felipe.w.pakua@gmail.com>
2021-04-15 19:22:15 -0300

    adding static method ehAutenticavel to check instace type of the parameter in the method login

--- js/funcionarios/SistemaAutenticacao.js
+++ js/funcionarios/SistemaAutenticacao.js
@@ -1,5 +1,17 @@
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
+        /*
+        essa linha verifica se existe alguma instancia de uma chave chamada "autenticar" dentro do 
+        parametro que passamos, e se essa instancia é tipo Function,  se tiver, ela retorna True e o 
+        metodo estatico Login pode  
+        */
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,23 +1,37 @@
 import{Cliente} from "./Cliente.js";
+import { Diretor } from "./funcionarios/Diretor.js";
+import { Gerente } from "./funcionarios/Gerente.js";
+import { SistemaAutenticacao } from "./funcionarios/SistemaAutenticacao.js";
 import{ContaCorrente} from "./tipoConta/ContaCorrente.js";
 import{ContaPoupanca} from "./tipoConta/ContaPoupanca.js";
 import { ContaSalario } from "./tipoConta/ContaSalario.js";
 
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("José", 5000, 12378945601);
+gerente.cadastrarSenha("123");
+
+SistemaAutenticacao.login(diretor, "123456789");
+
 const conta1 = new ContaCorrente(12, new Cliente("Ricardo",11122233309),1001);
-
 const conta2 = new ContaCorrente(0, new Cliente("Alice",88822233309),1001);
-
 const contaPoupancaRicardo = new ContaPoupanca(20, conta1._cliente, conta1._agencia);
-
 const contaSalarioAlice = new ContaSalario(0, conta2._cliente, conta2._agencia);
 
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456")
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123")
 conta1.depositar(500);
 conta1.transferir(250, conta2);
 conta2.depositar(350.25);
 
-console.log(conta1);
-console.log(conta2);
-console.log(contaPoupancaRicardo);
-console.log(contaSalarioAlice);
-console.log(conta1.sacar(200).toFixed(2));
-console.log(contaPoupancaRicardo.sacar(10));
+console.log(diretor);
+console.log(gerente);
+console.log(diretorEstaLogado, gerenteEstaLogado);
+
+// console.log(conta1);
+// console.log(conta2);
+// console.log(contaPoupancaRicardo);
+// console.log(contaSalarioAlice);
+// console.log(conta1.sacar(200).toFixed(2));
+// console.log(contaPoupancaRicardo.sacar(10));
